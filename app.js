150c5caf4c687e3e7bb1fbcf1289ebad6eb0b28c
everlastday <eastside@bigmir.net>
2019-03-12 22:33:22 +0200

    install mongoose & connect

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('MongoDB Connected...');
+}).catch(err => console.log(err));
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
